7765b951af01dcc0faf3ab9a681d390ace154a42
Paul Huske <pbhuske@gmail.com>
2018-02-07 14:07:47 -0900

    Update test.cpp
    Converted the Printing of Squares to a function that can be called to produce squares.

--- test.cpp
+++ test.cpp
@@ -5,14 +5,18 @@
 using std::cout;
 using std::endl;
 
+int Square( int num)
+{
+    return  num*num;
+}
+
 int main() {
     cout << "Carpe Diem!\n";
    cout<< " Paul wants to add a new line of code"<< endl;
     
-    for (int i = 1; i<10; i++)
-    {
-     cout << i*i << "\t";   
-    }
-    
+ for( int i = 1; i<10; i++)
+ {
+  cout<< Square(i) << "   ";   
+ }
     return 0;
  }
