5269d612759fde23fbdb58ed4ee03bfa5b67c75f
Paul Huske <pbhuske@gmail.com>
2018-02-07 14:08:52 -0900

    Update test.cpp
    Made changes should conflict now

--- test.cpp
+++ test.cpp
@@ -6,13 +6,13 @@ using std::cout;
 using std::endl;
 
 int main() {
-    cout << "Carpe Diem!\n";
-   cout<< " Paul wants to add a new line of code"<< endl;
+ 
     
     for (int i = 1; i<10; i++)
     {
      cout << i*i << "\t";   
     }
+    cout<< endl<< "Trying to Create a Merge Conflict";
     
     return 0;
  }
