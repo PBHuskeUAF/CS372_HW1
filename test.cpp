ab902522194d7f4b89002c9af084247b8382798f
Paul Huske <pbhuske@gmail.com>
2018-02-07 13:30:48 -0900

    Update test.cpp
    New text has been added to be printed.

--- test.cpp
+++ test.cpp
@@ -6,8 +6,13 @@ using std::cout;
 using std::endl;
 
 int main() {
-    cout << "Hello, World!\n";
-   
+    cout << "Carpe Diem!\n";
+   cout<< " Paul wants to add a new line of code"<< endl;
+    
+    for (int i = 1; i<10; i++)
+    {
+     cout << i*i << "\t";   
+    }
     
     return 0;
  }
